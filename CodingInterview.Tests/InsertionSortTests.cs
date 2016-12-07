6cd78c111588b4b9e055b0cfb159f00d82719bd7
Dmytro Bogatov <dmytro@dbogatov.org>
2016-12-06 20:13:05 -0500

    Cahnges overview:
    Add InsertionSort.

--- CodingInterview.Tests/InsertionSortTests.cs
+++ CodingInterview.Tests/InsertionSortTests.cs
@@ -1,36 +1,35 @@
 ﻿// 
 //
-// This file - BubleSortTests.cs was created by Dmytro Bogatov (dmytro@dbogatov.org)
-// on 12/6/2016, 7:28 PM
-
+// This file - InsertionSortTests.cs was created by Dmytro Bogatov (dmytro@dbogatov.org)
+// on 12/6/2016, 7:45 PM
 using NUnit.Framework;
 
 namespace CodingInterview.Tests
 {
 	[TestFixture]
-	public class BubleSortTests
+	public class InsertionSortTests
 	{
 		[Test]
 		public void TestCases()
 		{
 			Assert.AreEqual(
 				new int[] { 2, 5, 6, 8, 9 },
-				BubleSort.Sort(new int[] { 5, 2, 6, 8, 9 })
+				InsertionSort.Sort(new int[] { 5, 2, 6, 8, 9 })
 			);
 
 			Assert.AreEqual(
 				new int[] { 1 },
-				BubleSort.Sort(new int[] { 1 })
+				InsertionSort.Sort(new int[] { 1 })
 			);
 
 			Assert.AreEqual(
 				new int[] { },
-				BubleSort.Sort(new int[] { })
+				InsertionSort.Sort(new int[] { })
 			);
 
 			Assert.AreEqual(
 				new int[] { 5, 5, 6 },
-				BubleSort.Sort(new int[] { 5, 6, 5 })
+				InsertionSort.Sort(new int[] { 5, 6, 5 })
 			);
 		}
 	}
